154ef97d76d0e69e9b6c768fb755774f895deeb5
hcevensen <hcevensen@gmail.com>
2025-01-20 07:14:07 +0100

    dev env testing

--- cmd/main.go
+++ cmd/main.go
@@ -18,10 +18,8 @@ import (
 func main() {
 	log.Println("Starting Helm janitor process...")
 
-	// Initialize Kubernetes client and Helm settings
 	k8sClient, settings := initializeKubernetesClient()
 
-	// Allowed Helm charts
 	allowedCharts := []string{
 		"vscode-python",
 		"rstudio",
@@ -32,7 +30,7 @@ func main() {
 		"datadoc",
 	}
 
-	gocron.Every(1).Day().At("20:00").Do(func() {
+	gocron.Every(1).Day().At("07:45").Do(func() {
 		log.Println("Running 'Uninstall failed releases' job...")
 		if err := uninstall.UninstallFailedReleases(k8sClient, settings, allowedCharts, "user-ssb-"); err != nil {
 			log.Printf("Error during 'Uninstall failed releases' job: %v", err)
@@ -41,7 +39,7 @@ func main() {
 		}
 	})
 
-	gocron.Every(1).Day().At("22:00").Do(func() {
+	gocron.Every(1).Day().At("08:10").Do(func() {
 		log.Println("Running 'Suspend user services' job...")
 		if err := suspend.SuspendReleases(k8sClient, settings, allowedCharts, "user-ssb-"); err != nil {
 			log.Printf("Error during 'Suspend user services' job: %v", err)
@@ -59,15 +57,13 @@ func initializeKubernetesClient() (*kubernetes.Clientset, *cli.EnvSettings) {
 	var config *rest.Config
 	var err error
 
-	// Try using local kubeconfig
 	kubeconfig := os.Getenv("KUBECONFIG")
 	if kubeconfig == "" {
-		kubeconfig = clientcmd.RecommendedHomeFile // Default kubeconfig path
+		kubeconfig = clientcmd.RecommendedHomeFile
 	}
 	config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Printf("Could not use local kubeconfig: %v. Trying in-cluster configuration...", err)
-		// Fall back to in-cluster config
 		config, err = rest.InClusterConfig()
 		if err != nil {
 			log.Fatalf("Could not use in-cluster configuration: %v", err)
