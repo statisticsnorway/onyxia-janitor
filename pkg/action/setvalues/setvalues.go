fc0cc7a10acbe5bc08b82d2ec9bcb9bcdb548017
nicolst <35232838+nicolst@users.noreply.github.com>
2025-06-10 15:51:41 +0200

    feat: add arbitrary value mapper action

---
+++ pkg/action/setvalues/setvalues.go
@@ -0,0 +1,83 @@
+package setvalues
+
+import (
+	"context"
+	"log/slog"
+	"onyxia-janitor/pkg/onyxia"
+
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/cli"
+	"k8s.io/client-go/kubernetes"
+)
+
+type valueSetter struct {
+	catalogs     onyxia.Catalogs
+	kubernetes   *kubernetes.Clientset
+	helmSettings *cli.EnvSettings
+	valuesMapper func(swr onyxia.ServiceWithRelease) (map[string]any, error)
+}
+
+func New(client *kubernetes.Clientset, settings *cli.EnvSettings, catalogs onyxia.Catalogs, valuesMapper func(swr onyxia.ServiceWithRelease) (map[string]any, error)) *valueSetter {
+	return &valueSetter{
+		kubernetes:   client,
+		helmSettings: settings,
+		catalogs:     catalogs,
+		valuesMapper: valuesMapper,
+	}
+}
+
+// Process sets .global.suspend=true for the given service.
+func (s *valueSetter) Process(ctx context.Context, swr onyxia.ServiceWithRelease) error {
+	log := swr.AddToLogger(slog.Default())
+
+	actionConfig := new(action.Configuration)
+	s.helmSettings.SetNamespace(swr.Release.Namespace)
+	if err := actionConfig.Init(s.helmSettings.RESTClientGetter(), swr.Release.Namespace, "", log.Debug); err != nil {
+		log.Error("error initializing config for release suspension", "err", err)
+		return err
+	}
+	log.Debug("attempting to suspend release")
+
+	values, err := s.valuesMapper(swr)
+	if err != nil {
+		slog.Error("error mapping values to set", "err", err)
+		return err
+	}
+
+	chartPathOptions := action.ChartPathOptions{
+		// We here assume that catalog existence has been checked
+		// in a previous filter stage.
+		RepoURL: s.catalogs[swr.Service.Catalog].Url,
+		Version: swr.Release.Chart.Metadata.Version,
+	}
+	chartPath, err := chartPathOptions.LocateChart(swr.Release.Chart.Metadata.Name, s.helmSettings)
+	if err != nil {
+		slog.Error("error locating chart for release", "err", err)
+		return err
+	}
+
+	chart, err := loader.Load(chartPath)
+	if err != nil {
+		log.Error("error loading chart", "err", err)
+		return err
+	}
+
+	upgradeAction := action.NewUpgrade(actionConfig)
+	upgradeAction.Namespace = swr.Release.Namespace
+	upgradeAction.Version = swr.Release.Chart.Metadata.Version
+	upgradeAction.ReuseValues = true
+
+	_, err = upgradeAction.Run(swr.Release.Name, chart, values)
+	if err != nil {
+		log.Error("error upgrading release", "err", err)
+		return err
+	}
+
+	log.Info("successfully suspended release")
+	return nil
+}
+
+func (s *valueSetter) Finish(ctx context.Context) error {
+	return nil
+}
